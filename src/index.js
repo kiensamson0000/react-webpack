19cd6259f1ea720fda3f603ac7e2c5ab8b8e8218
Khuat Huy Kien <kienkh99@gmail.com>
2022-03-04 14:42:14 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+    return (
+        <div>
+            <h1>Hello Everyone!</h1>
+            <h2>This's template init a webpack</h2>
+            <h3>Thanks you very much!</h3>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
